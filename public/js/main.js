151a31f744b7646665c8f12b9a3d247be90acd81
Michael Herman <hermanmu@gmail.com>
2014-01-17 16:41:27 -0700

    heart click

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,16 @@
-$(document).ready(function() {
-  var imageUrl = $('body').find('img').attr('src');
-  var imgTag = ["<img src='", imageUrl, "'>"].join('');
-  for (var i=1;i<40;i++) {$('#images').append(imgTag);}
+// $(document).ready(function() {
+//   var imageUrl = $('body').find('img').attr('src');
+//   var imgTag = ["<img src='", imageUrl, "'>"].join('');
+//   for (var i=1;i<40;i++) {$('#images').append(imgTag);}
+// });
+
+$(function() {
+
+  console.log("whee!")
+
+  // event handler
+  $(".heart-click").click(function(e) {
+  	$("#heart-append").append("<span class='heart-click heart glyphicon glyphicon-heart'></span>");
+  });
+
 });
