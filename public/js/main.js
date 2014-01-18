97621072723c536234a26e91586e34c241f2ceb4
Michael Herman <hermanmu@gmail.com>
2014-01-18 10:32:47 -0700

    canvas

--- public/js/main.js
+++ public/js/main.js
@@ -4,13 +4,13 @@
 //   for (var i=1;i<40;i++) {$('#images').append(imgTag);}
 // });
 
-$(function() {
+// $(function() {
 
-  console.log("whee!")
+//   console.log("whee!")
 
-  // event handler
-  $(".heart-click").click(function(e) {
-  	$("#heart-append").append("<span class='heart-click heart glyphicon glyphicon-heart'></span>");
-  });
+//   // event handler
+//   $(".heart-click").click(function(e) {
+//   	$("#heart-append").append("<span class='heart-click heart glyphicon glyphicon-heart'></span>");
+//   });
 
-});
+// });
