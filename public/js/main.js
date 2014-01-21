bf452e25e4684c0139506fb0a193692a1d623b60
Michael Herman <hermanmu@gmail.com>
2014-01-21 14:35:45 -0700

    countdown

--- public/js/main.js
+++ public/js/main.js
@@ -14,3 +14,15 @@
 //   });
 
 // });
+
+$(function() {
+
+  var endDate = "January 14, 2014 16:46:00";
+  $('.end').countdown({
+    date: endDate,
+    render: function(data) {
+      $(this.el).html("<div>" + this.leadingZeros(data.days, 3) + " <span>days</span></div><div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div><div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div><div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
+    }
+  });  
+
+});
